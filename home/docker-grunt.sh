7880e827c2b677bc44eca571d2b42bd2294cba69
Laurent Goderre <laurent.goderre@gmail.com>
2016-10-20 13:34:03 -0400

    Updated the docker grunt script

--- home/docker-grunt.sh
+++ home/docker-grunt.sh
@@ -2,9 +2,11 @@ docker run \
     --net=host \
     -it --rm \
     -v "$(pwd)":/data \
+    -v $HOME/.gitconfig:/root/.gitconfig \
+    -v $HOME/.npmrc:/root/.npmrc \
     -e http_proxy=$http_proxy \
     -e https_proxy=$https_proxy \
     -e HTTP_PROXY=$HTTP_PROXY \
     -e HTTPS_PROXY=$HTTPS_PROXY \
     digitallyseamless/nodejs-bower-grunt \
-    /bin/bash -c "npm install; grunt"
+    bash
